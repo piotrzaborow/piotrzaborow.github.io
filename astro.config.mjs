15963ed859984bfce12e72ecd57b064fec9d736a
Piotr Zaborowski <piotrzaborow@gmail.com>
2024-05-02 09:03:58 +0200

    chore: add vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind()]
+  site: 'https://blog.zaborowski.dev',
+  integrations: [mdx(), sitemap(), tailwind()],
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
 });
