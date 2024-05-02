f685ba5cacabb7d7e5d489439966d5b54b183217
Piotr Zaborowski <piotrzaborow@gmail.com>
2024-05-02 09:06:27 +0200

    fix: add Vercel Astro plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://blog.zaborowski.dev',
   integrations: [mdx(), sitemap(), tailwind()],
-  output: 'server',
+  output: "server",
   adapter: vercel({
     webAnalytics: { enabled: true }
-  }),
+  })
 });
